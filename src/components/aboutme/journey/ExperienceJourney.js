dd483c8ce65df3b2019dc05735bfc5dcbc03c59a
Jenesha20 <jeneshamalars@gmail.com>
2024-12-03 13:39:19 +0530

    Portfolio

---
+++ src/components/aboutme/journey/ExperienceJourney.js
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import PersonIcon from "@mui/icons-material/Person";
+import { Link } from "react-router-dom";
+import StarIcon from "@mui/icons-material/Star";
+import Flip from "react-reveal/Flip";
+
+function ExperienceJourney() {
+  return (
+    <div className="mt-5">
+      <div>
+        <Link className="btn btn-primary m-1" to="/educationjourney">
+          Educational Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/experiencejourney">
+          Experience Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/technicaljourney">
+          Technical Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/projectjourney">
+          Project Journey
+        </Link>
+      </div>
+      <div className="mt-5">
+        <Flip top cascade>
+          <h1>Experience Journey</h1>
+        </Flip>
+        </div>
+<VerticalTimeline>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+    contentArrowStyle={{ borderRight: "7px solid rgb(78 22 112)" }}
+    date="Ongoing"
+    iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+    icon={<PersonIcon />}
+  >
+    <h5 className="vertical-timeline-element-title">PunchBiz</h5>
+    <h6 className="vertical-timeline-element-subtitle mt-2">Full Stack Data Scientist | Intern</h6>
+    <p>• Developed a responsive and real-time intruder detection system using Flask for the backend and React Native for the mobile and web application.</p>
+    <p>• Implemented person detection using YOLO and integrated email notifications for intruder alerts, enhancing security monitoring functionality.</p>
+  </VerticalTimelineElement>
+  
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+    contentArrowStyle={{ borderRight: "7px solid rgb(78 22 112)" }}
+    date="May 2024 – July 2024"
+    iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+    icon={<PersonIcon />}
+  >
+    <h5 className="vertical-timeline-element-title">Intel</h5>
+    <h6 className="vertical-timeline-element-subtitle mt-2">Full Stack Developer | Intern</h6>
+    <p>• Developed a responsive and dynamic website using the MERN stack (MongoDB, Express.js, React, Node.js).</p>
+    <p>• Implemented user authentication and CRUD operations to enhance website functionality.</p>
+  </VerticalTimelineElement>
+
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+    contentArrowStyle={{ borderRight: "7px solid rgb(78 22 112)" }}
+    date="March - April 2024"
+    iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+    icon={<PersonIcon />}
+  >
+    <h5 className="vertical-timeline-element-title">StartFleet</h5>
+    <h6 className="vertical-timeline-element-subtitle mt-2">Data Analyst | Intern</h6>
+    <p>• Performed in-depth data analysis and derived actionable insights using Power BI.</p>
+    <p>• Designed and delivered interactive dashboards and comprehensive reports to enhance data visualization and interpretation.</p>
+  </VerticalTimelineElement>
+
+  <VerticalTimelineElement
+    iconStyle={{ background: "#fbd9ad", color: "rgb(78 22 112)" }}
+    icon={<StarIcon />}
+  />
+</VerticalTimeline>
+</div>
+
+  );
+}
+
+export default ExperienceJourney;
---
+++ src/components/aboutme/journey/ProjectJourney.js
@@ -0,0 +1,170 @@
+import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { Link } from "react-router-dom";
+import StarIcon from "@mui/icons-material/Star";
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import CssIcon from '@mui/icons-material/Css';
+import HtmlIcon from '@mui/icons-material/Html';
+import CodeIcon from '@mui/icons-material/Code';
+import Flip from "react-reveal/Flip";
+import { BiBarChartAlt2 } from "react-icons/bi"; // Power BI
+import { FaBrain } from "react-icons/fa"; // LLM or Machine Learning
+import { AiOutlineRobot } from "react-icons/ai"; // Deep Learning
+
+function ProjectJourney() {
+  return (
+    <div className="mt-5">
+      <div>
+        <Link className="btn btn-primary m-1" to="/educationjourney">
+          Educational Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/experiencejourney">
+          Experience Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/technicaljourney">
+          Technical Journey
+        </Link>
+        <Link className="btn btn-primary m-1" to="/projectjourney">
+          Project Journey
+        </Link>
+      </div>
+      <div className="mt-5">
+        <Flip top cascade>
+          <h1>Project Journey</h1>
+        </Flip>
+      </div>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<AiOutlineRobot />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Disease Symptom Prediction
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<AiOutlineRobot />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Dogs Breed Identification
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<AiOutlineRobot />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Traffic Sign Detection
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<BiBarChartAlt2 />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Supply Chain Management for Cars
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<BiBarChartAlt2 />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Patient Data Analysis
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<BiBarChartAlt2 />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          HR Data Analysis
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<BiBarChartAlt2 />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Ecommerce Data Analysis
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<CodeIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Intel Housing Services
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<CodeIcon />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Expense Tracker
+          </h3>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(78 22 112)" }}
+          date=""
+          iconStyle={{ background: "rgb(78 22 112)", color: "#fbd9ad" }}
+          icon={<FaBrain />}
+        >
+          <h3 className="vertical-timeline-element-title">
+          Personalized Career Development
+          </h3>
+        </VerticalTimelineElement>
+        
+        <VerticalTimelineElement
+          iconStyle={{ background: "#fbd9ad", color: "rgb(78 22 112)" }}
+          icon={<StarIcon />}
+        />
+      </VerticalTimeline>
+    </div>
+  );
+}
+
+export default ProjectJourney;
